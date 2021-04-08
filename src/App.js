81d8ba3d1db26608d1dfdc3aa0962f7bc4d6115d
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-04-08 20:38:51 +0700

    book list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div id="main">
       <h1>Reading List</h1>
+      <BookList/>
     </div>
   );
 }
