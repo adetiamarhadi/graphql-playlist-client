a3948f6dca07b7ace6f36a8091fee0ab020d627e
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-04-12 10:33:30 +0700

    making queries from react

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,15 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+const getBooksQuery = gql`{
+    books {
+        id
+        name
+    }
+}`;
+
 function BookList() {
+    useQuery(getBooksQuery);
     return (
         <div>
             <ul id="book-list">
