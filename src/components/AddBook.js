1c573e22c60fd8266bf0074d295f370551cc4135
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-04-16 11:19:51 +0700

    updating component state

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,5 +1,15 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { getAuthorsQuery } from "../queries/queries";
+
+// constructor(props) {
+//     super(props);
+//     this.state = {
+//         name: '',
+//         genre: '',
+//         authorId: ''
+//     };
+// }
 
 function DisplayAuthors() {
     const q = useQuery(getAuthorsQuery);
@@ -16,22 +26,57 @@ function DisplayAuthors() {
     }
 }
 
+// function submitForm(e) {
+//     e.preventDefault();
+//     console.log(this.state);
+// };
+
 function AddBook() {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         name: '',
+    //         genre: '',
+    //         authorId: ''
+    //     };
+    // }
+    // const [name, setName] = useState('');
+    // const [genre, setGenre] = useState('');
+    // const [authorId, setAuthorId] = useState('');
+    const [data, setData] = useState({
+        name: '',
+        genre: '',
+        authorId: ''
+    });
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log(data);
+    };
+
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        data[name] = value;
+        setData(data);
+    };
+
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={ submitForm }>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" />
+                <input type="text" name="name" onChange={ handleChange } />
             </div>
 
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" />
+                <input type="text" name="genre" onChange={ handleChange } />
             </div>
 
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select name="authorId" onChange={ handleChange }>
                     <option>Select author</option>
                     <DisplayAuthors/>
                 </select>
